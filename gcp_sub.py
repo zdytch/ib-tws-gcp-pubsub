74e89992d26c02007bc8d5a95d41184ed490469d
Alexey Luzin <zdytch@gmail.com>
2023-01-25 22:06:04 +0200

    Replace gcloud-aio-pubsub with google-cloud-pubsub

--- gcp_sub.py
+++ gcp_sub.py
@@ -1,22 +1,26 @@
-from gcloud.aio.pubsub import SubscriberClient, subscribe
-from typing import Callable
 import os
+from google.cloud import pubsub_v1
+from concurrent.futures import TimeoutError
 
-PUBSUB_PROJECT = os.environ['PUBSUB_PROJECT']
-PUBSUB_TOPIC = os.environ['PUBSUB_TOPIC']
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'gcp_key.json'
+SUBSCRIPTION_PATH = 'projects/tsa-test-314819/subscriptions/test_sub'
 
 
-async def subscribe_signals(handler: Callable) -> None:
-    subscriber = SubscriberClient()
+async def subscribe_signals() -> None:
+    subscriber = pubsub_v1.SubscriberClient()
+    streaming_pull_future = subscriber.subscribe(SUBSCRIPTION_PATH, callback=callback)
 
-    await subscribe(
-        f'projects/{PUBSUB_PROJECT}/subscriptions/{PUBSUB_TOPIC}',
-        handler,
-        subscriber,
-        num_producers=1,
-        max_messages_per_producer=100,
-        ack_window=0.3,
-        num_tasks_per_consumer=1,
-        enable_nack=True,
-        nack_window=0.3,
-    )
+    print(f'Listening for messages on {SUBSCRIPTION_PATH}..\n')
+
+    with subscriber:
+        try:
+            streaming_pull_future.result()
+
+        except TimeoutError:
+            streaming_pull_future.cancel()
+            streaming_pull_future.result()
+
+
+def callback(message: pubsub_v1.subscriber.message.Message) -> None:
+    print(f'Received {message}.')
+    message.ack()
