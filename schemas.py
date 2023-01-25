3f212ba67ad741a353ab1004edbfa24d14c47bf4
Alexey Luzin <zdytch@gmail.com>
2023-01-25 20:02:28 +0200

    Add pydantic schema for Order

---
+++ schemas.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel
+from enum import Enum
+from decimal import Decimal
+
+
+class Exchange(Enum):
+    NYSE = 'NYSE'
+    NASDAQ = 'NASDAQ'
+
+
+class Side(Enum):
+    BUY = 'BUY'
+    SELL = 'SELL'
+
+
+class OrderType(Enum):
+    LIMIT = 'LMT'
+    STOP = 'STP'
+    MARKET = 'MKT'
+
+
+class Order(BaseModel):
+    symbol: str
+    exchange: Exchange
+    side: Side
+    type: OrderType
+    size: int
+    price: Decimal = Decimal('0.0')
