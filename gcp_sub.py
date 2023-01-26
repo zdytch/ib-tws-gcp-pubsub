6635143427a72c31c926bd90df1415638154b23d
Alexey Luzin <zdytch@gmail.com>
2023-01-26 02:46:25 +0200

    Subscribe to GCPConnector callbacks

--- gcp_sub.py
+++ gcp_sub.py
@@ -1,26 +1,48 @@
 import os
 from google.cloud import pubsub_v1
 from concurrent.futures import TimeoutError
+from json import loads, JSONDecodeError
+from schemas import CallbackData, SubmitData
+from typing import Callable
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'gcp_key.json'
 SUBSCRIPTION_PATH = 'projects/tsa-test-314819/subscriptions/test_sub'
 
 
-async def subscribe_signals() -> None:
-    subscriber = pubsub_v1.SubscriberClient()
-    streaming_pull_future = subscriber.subscribe(SUBSCRIPTION_PATH, callback=callback)
+class GCPConnector:
+    def __init__(self):
+        self._subscriber = pubsub_v1.SubscriberClient()
 
-    print(f'Listening for messages on {SUBSCRIPTION_PATH}..\n')
+    def set_data_callback(self, data_callback: Callable[[CallbackData], None]) -> None:
+        self.data_callback = data_callback
 
-    with subscriber:
-        try:
-            streaming_pull_future.result()
+    def subscribe(self) -> None:
+        streaming_pull_future = self._subscriber.subscribe(
+            SUBSCRIPTION_PATH, callback=self._subscriber_callback
+        )
 
-        except TimeoutError:
-            streaming_pull_future.cancel()
-            streaming_pull_future.result()
+        print(f'Listening for messages on {SUBSCRIPTION_PATH}..\n')
 
+        with self._subscriber:
+            try:
+                streaming_pull_future.result()
 
-def callback(message: pubsub_v1.subscriber.message.Message) -> None:
-    print(f'Received {message}.')
-    message.ack()
+            except (TimeoutError, KeyboardInterrupt):
+                streaming_pull_future.cancel()
+                streaming_pull_future.result()
+
+    def _subscriber_callback(
+        self, message: pubsub_v1.subscriber.message.Message
+    ) -> None:
+        message.ack()
+
+        if message.data:
+            try:
+                json = loads(message.data)
+                data = SubmitData(**json)
+
+                if hasattr(self, 'data_callback'):
+                    self.data_callback(data)
+
+            except JSONDecodeError as error:
+                print(error)
