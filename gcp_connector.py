80cb1577b4f83863df4e9e46071f0ac33c2615bf
Alexey Luzin <zdytch@gmail.com>
2023-01-30 17:24:52 +0200

    Use GCPConnector class

--- gcp_connector.py
+++ gcp_connector.py
@@ -1,3 +1,4 @@
+from asyncio import create_task
 from gcloud.aio.pubsub import SubscriberClient, SubscriberMessage, subscribe
 from json import loads, JSONDecodeError
 from pydantic import ValidationError
@@ -6,29 +7,32 @@ from settings import GCP_PROJECT_ID, GCP_SUBSCRIPTION_ID
 from typing import Callable
 from loguru import logger
 
-_data_callback: Callable[[CallbackData], None]
 
+class GCPConnector:
+    def run(self) -> None:
+        create_task(self._listen())
 
-async def listen(data_callback: Callable[[CallbackData], None]) -> None:
-    global _data_callback
-    _data_callback = data_callback
+    def set_data_callback(self, data_callback: Callable[[CallbackData], None]) -> None:
+        self.data_callback = data_callback
 
-    subscription_path = f'projects/{GCP_PROJECT_ID}/subscriptions/{GCP_SUBSCRIPTION_ID}'
-    subscriber = SubscriberClient()
+    async def _listen(self) -> None:
+        subscription_path = (
+            f'projects/{GCP_PROJECT_ID}/subscriptions/{GCP_SUBSCRIPTION_ID}'
+        )
+        subscriber = SubscriberClient()
 
-    logger.debug(f'Listening for messages on {subscription_path}...')
+        logger.debug(f'Listening for messages on {subscription_path}...')
 
-    await subscribe(subscription_path, _subscriber_callback, subscriber)
+        await subscribe(subscription_path, self._subscriber_callback, subscriber)
 
+    async def _subscriber_callback(self, message: SubscriberMessage) -> None:
+        if message.data:
+            try:
+                json = loads(message.data)
+                data = SubmitData(**json)
 
-async def _subscriber_callback(message: SubscriberMessage) -> None:
-    if message.data:
-        try:
-            json = loads(message.data)
-            data = SubmitData(**json)
+                if hasattr(self, 'data_callback'):
+                    self.data_callback(data)
 
-            if _data_callback:
-                _data_callback(data)
-
-        except (JSONDecodeError, ValidationError) as error:
-            logger.debug(error)
+            except (JSONDecodeError, ValidationError) as error:
+                logger.debug(error)
