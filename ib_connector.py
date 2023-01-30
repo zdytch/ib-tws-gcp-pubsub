c303a1ae96da11a779ea356019fca48d92421fa7
Alexey Luzin <zdytch@gmail.com>
2023-01-30 13:12:50 +0200

    Add IBConnector._create_order()

--- ib_connector.py
+++ ib_connector.py
@@ -1,5 +1,5 @@
-from ib_insync import IB, Order as IBOrder, Stock, Trade
-from schemas import CallbackData, SubmitData, StatusData, OrderStatus
+from ib_insync import IB, Order, Stock, Trade
+from schemas import CallbackData, SubmitData, StatusData, OrderStatus, OrderType
 from decimal import Decimal
 from uuid import UUID
 from typing import Callable
@@ -19,17 +19,9 @@ class IBConnector:
 
         if self._ib.isConnected():
             contract = Stock(data.symbol, f'SMART:{data.exchange.value}', 'USD')
-            ib_order = IBOrder(
-                orderId=self._ib.client.getReqId(),
-                orderRef=str(data.id),
-                action=data.side.value,
-                orderType=data.type.value,
-                totalQuantity=float(data.size),
-                tif='DAY',
-                transmit=True,
-            )
+            order = self._create_order(data)
 
-            self._ib.placeOrder(contract, ib_order)
+            self._ib.placeOrder(contract, order)
 
     async def _connect(self):
         if not self._ib.isConnected():
@@ -38,6 +30,25 @@ class IBConnector:
 
             except Exception as error:
                 logger.debug(error)
+
+    def _create_order(self, data: SubmitData) -> Order:
+        order = Order(
+            orderId=self._ib.client.getReqId(),
+            orderRef=str(data.id),
+            action=data.side.value,
+            orderType=data.type.value,
+            totalQuantity=float(data.size),
+            tif='DAY',
+            transmit=True,
+        )
+
+        if data.type == OrderType.LIMIT:
+            order.lmtPrice = float(data.price)
+
+        elif data.type == OrderType.STOP:
+            order.auxPrice = float(data.price)
+
+        return order
 
     def _order_status_callback(self, trade: Trade) -> None:
         logger.debug(f'IB trade changed: {trade}')
