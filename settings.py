1850262f6378a903373508e999cf8940d425bee9
Alexey Luzin <zdytch@gmail.com>
2023-01-30 18:22:41 +0200

    Fix topic settings

--- settings.py
+++ settings.py
@@ -2,5 +2,6 @@ import os
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'gcp_key.json'
 
-GCP_PROJECT_ID = 'tsa-test-314819'
-GCP_SUBSCRIPTION_ID = 'test_sub'
+GCP_PROJECT_ID = 'peppy-coda-376121'
+GCP_STATUS_TOPIC_ID = 'status-topic'
+GCP_SUBMIT_TOPIC_SUB_ID = 'submit-topic-sub'
